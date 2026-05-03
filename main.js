faa2bc2b33ad5f1811c7d96cd21445ff4431996b
Marce-Lima <166259893+Marce-Lima@users.noreply.github.com>
2026-05-03 01:00:12 -0300

    ele aparece

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.mod
 import { criarBanco } from './objects/banco.js';
 import { CriarFlocoDeNeve } from './objects/flocoDeNeve.js';
 import { criarChapeu } from './objects/chapeu.js';
+import { criarBoneco } from './objects/bonecoDeNeve.js';
 
 // Cena, câmera, renderer
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xdde3e8);
+scene.background = new THREE.Color(0x9ad0f0); //só mudei pra enxergar o boneco melhor, depois mudamos
 const chaoGeo = new THREE.PlaneGeometry(2000, 2000);
 const chaoMat = new THREE.MeshStandardMaterial({ color: 0xb0b8c1 });
 const chao = new THREE.Mesh(chaoGeo, chaoMat);
@@ -13,11 +14,11 @@ chao.rotation.x = -Math.PI / 2; // deita o plano
 chao.position.y = 0;
 scene.add(chao);
 
-const camera1 = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10000);
+const camera1 = new THREE.PerspectiveCamera(30, innerWidth / innerHeight, 0.1, 10000);//era 75 de zoom
 camera1.position.set(300, 200, 400);
 camera1.lookAt(0, 50, 0);
 
-const camera2 = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10000);
+const camera2 = new THREE.PerspectiveCamera(30, innerWidth / innerHeight, 0.1, 10000);
 camera2.position.set(0, 100, 500);
 camera2.lookAt(0, 50, 0);
 
@@ -44,8 +45,9 @@ scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 // Seu objeto
 criarBanco(scene);
 criarChapeu(scene, { x: 150, y: 72, z: 200 });
-
 const floco = new CriarFlocoDeNeve(scene);
+const frederico = criarBoneco(scene);
+scene.add(frederico);
 
 // Loop de renderização
 function animate() {
--- objects/bonecoDeNeve.js
+++ objects/bonecoDeNeve.js
@@ -2,45 +2,72 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.mod
 
 export function criarBoneco(scene){
 
-    //corpo do boneco (três bolas de neve)
-    const geometry1 = new THREE.SphereGeometry(1, 32, 32);
-    const material1 = new THREE.MeshBasicMaterial( { color: 0x8ff7f0} );
+    const boneco = new THREE.Group();
+    const cabeca = new THREE.Group();
+    const corpo = new THREE.Group();
+    const braco1 = new THREE.Group();
+    const braco2 = new THREE.Group();
+
+    const neve = new THREE.MeshBasicMaterial({color: 0xcedfdf});
+    const madeira = new THREE.MeshBasicMaterial({color: 0x60360e});
+    const mat_cenoura = new THREE.MeshBasicMaterial({color: 0xd47217});
+    const pedra = new THREE.MeshBasicMaterial({color: 0x393535});
+
+    //corpo do boneco (três bolas de neve) as cores sao só para melhor visualização
+    const geometry1 = new THREE.SphereGeometry(35, 32, 32);
+    const material1 = new THREE.MeshBasicMaterial({color: 0x8ff7f0});
     const sphere1 = new THREE.Mesh( geometry1, material1 );
-    sphere1.position.set(0,-1.5,0);
-    scene.add( sphere1 );
+    sphere1.position.set(0, 130, 0);
+    scene.add(sphere1);
 
-    const geometry2 = new THREE.SphereGeometry(1, 32, 32);
-    const material2 = new THREE.MeshBasicMaterial( { color: 0xe9f78f} );
+    const geometry2 = new THREE.SphereGeometry(40, 32, 32);
+    const material2 = new THREE.MeshBasicMaterial({color: 0xe9f78f});
     const sphere2 = new THREE.Mesh( geometry2, material2 );
-    sphere2.position.set(0, 0, 0);
-    scene.add( sphere2 );
+    sphere2.position.set(0, 80, 0);
+    scene.add(sphere2);
 
-    const geometry3 = new THREE.SphereGeometry(1, 32, 32);
-    const material3 = new THREE.MeshBasicMaterial( { color: 0xf08ff7} );
+    const geometry3 = new THREE.SphereGeometry(50, 32, 32);
+    const material3 = new THREE.MeshBasicMaterial({color: 0xf08ff7});
     const sphere3 = new THREE.Mesh( geometry3, material3 );
-    sphere3.position.set(0, 1.5, 0);
-    scene.add( sphere3 );
+    sphere3.position.set(0, 20, 0);
+    scene.add(sphere3);
     
     //nariz (cone)
-    const geometry4 = new THREE.ConeGeometry(0.2, 1, 32);
-    const material4 = new THREE.MeshBasicMaterial( { color: 0xffa500} );
+    const geometry4 = new THREE.ConeGeometry(5, 30, 32);
+    const material4 = new THREE.MeshBasicMaterial({color: 0xffa500});
     const cone = new THREE.Mesh( geometry4, material4 );
-    cone.position.set(0, 1.25, 1.25);
+    cone.position.set(0, 130, 45);
     cone.rotation.x = Math.PI / 2;
     cone.rotation.y = 0;
     cone.rotation.z = 0;
-    scene.add( cone );
+    scene.add(cone);
 
-    //bracinho1
-    const material6 = new THREE.LineBasicMaterial({ color: 0x8B4513 });
+    //bracinhos (tubos estranhos)
+    const material6 = new THREE.MeshBasicMaterial({ color: 0x8B4513 });
     const geometry6 = new THREE.CylinderGeometry(0.05, 0.05, 2);
-
     const stick1 = new THREE.Mesh(geometry6, material6);
 
     stick1.position.set(1, 0.25, 0);
     stick1.rotation.z = Math.PI / 4;
     scene.add(stick1);
 
+    /*
+    const geometry5 = new THREE.SphereGeometry(50, 32, 32);
+    const material3 = new THREE.MeshBasicMaterial({color: 0xf08ff7});
+    const sphere3 = new THREE.Mesh( geometry3, material3 );
+    sphere3.position.set(0, 20, 0);
+    scene.add(sphere3);
+    */
+
+    cabeca.add(sphere1);
+    cabeca.add(cone);
+    corpo.add(sphere2);
+    corpo.add(sphere3);
+
+    boneco.add(corpo);
+    boneco.add(cabeca);
+
+    return boneco;
 }
 
 //bracinho2
